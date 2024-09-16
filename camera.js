873b8c8a74193a6233159b7d7cbef312280f83a3
wh-hm <146697825+wh-hm@users.noreply.github.com>
2024-09-17 01:28:42 +0900

    Update camera.js

--- camera.js
+++ camera.js
@@ -7,21 +7,13 @@ const cameraInitSmartphoneSupport = () => {
     //スマホからの閲覧か
     const isMobile = navigator.userAgent.match(/iPhone|Android/);
 
-    const cameraSetting = {
-        audio: false,
-        video: {
-            //スマホの場合は縦横を逆に設定する
-            width: isMobile ? cameraHeight : cameraWidth,
-            height: isMobile ? cameraWidth : cameraHeight,
-            facingMode: "environment",
-        }
-    }
+    var media = navigator.mediaDevices.getUserMedia({
+        video: true;
+        audio: false;
+    });
 
-    navigator.mediaDevices.getUserMedia(cameraSetting)
-        .then((mediaStream) => {
-            video.srcObject = mediaStream;
-        })
-        .catch((err) => {
-            console.log(err.toString());
-        });
+    media.then((stream) => {
+        video.srcObject = stream;
+    });
+
 }
